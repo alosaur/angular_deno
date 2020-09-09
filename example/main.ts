eff3f62705eeeb8d855f954cae844c86697c4ebf
Jamaks <jamakrus@gmail.com>
2020-09-09 23:53:59 +0300

    fix fmt

--- example/main.ts
+++ example/main.ts
@@ -1,20 +1,23 @@
-import { CompilerFactory, enableProdMode } from '@angular/core';
-import { bootstrap, CommonEngine } from '../mod.ts';
-import { AppModule } from './app.module.ts';
+import { CompilerFactory, enableProdMode } from "@angular/core";
+import { bootstrap, CommonEngine } from "../mod.ts";
+import { AppModule } from "./app.module.ts";
 
 import "../reflect.ts";
-import 'zone.js';
+import "zone.js";
 
 const { readFile } = Deno;
 const decoder = new TextDecoder();
 
-const indexHtml = decoder.decode(await readFile('index.html'));
+const indexHtml = decoder.decode(await readFile("index.html"));
 
 enableProdMode();
 
 const ref: any = await bootstrap(AppModule, indexHtml);
 
-export const engine = new CommonEngine(ref.injector.get(CompilerFactory), AppModule);
+export const engine = new CommonEngine(
+  ref.injector.get(CompilerFactory),
+  AppModule,
+);
 
 // Render page
 
--- example/server.ts
+++ example/server.ts
@@ -1,26 +1,31 @@
 import { serve } from "https://deno.land/std@0.59.0/http/server.ts";
 
-import { CompilerFactory, enableProdMode } from '@angular/core';
-import { bootstrap, CommonEngine } from '../mod.ts';
-import { AppModule } from './app.module.ts';
+import { CompilerFactory, enableProdMode } from "@angular/core";
+import { bootstrap, CommonEngine } from "../mod.ts";
+import { AppModule } from "./app.module.ts";
 
 import "../reflect.ts";
-import 'zone.js';
+import "zone.js";
 
 const { readFile } = Deno;
 const decoder = new TextDecoder();
 
-const indexHtml = decoder.decode(await readFile('index.html'));
+const indexHtml = decoder.decode(await readFile("index.html"));
 
 enableProdMode();
 
 const ref: any = await bootstrap(AppModule, indexHtml);
 
-export const engine = new CommonEngine(ref.injector.get(CompilerFactory), AppModule);
+export const engine = new CommonEngine(
+  ref.injector.get(CompilerFactory),
+  AppModule,
+);
 
 const s = serve({ port: 8000 });
 
 for await (const req of s) {
-  const body: string = await engine.render({ document: indexHtml, url: req.url });
+  const body: string = await engine.render(
+    { document: indexHtml, url: req.url },
+  );
   req.respond({ body });
 }
